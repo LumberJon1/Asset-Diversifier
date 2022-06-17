8f5f92da91f1c8865e95f9520a1fcdf6068d026e
Jonathan W <jwilliams12117@gmail.com>
2022-06-16 17:53:54 -0700

    Outlined pseudocode

---
+++ Main.py
@@ -0,0 +1,26 @@
+
+import math
+
+# Define mock datasets
+
+# Define global variables
+
+# Define statistical functions
+
+# Function that iterates through the datasets and finds correlations
+
+# --- Main body of the program (genetic algorithm) ---
+
+    # Take user input on number of assets to include and target correlation
+
+    # Take variable number of assets and their historical prices
+
+    # Compute statistical functions and find correlation of different combinations
+    
+    # Randomly combine different assets using random weights
+    
+    # Assess fit of correlation to within acceptable error of target
+    
+    # Discard lowest quartile and re-run algorithm if outside of bounds
+    
+    # Return resulting successful mix with proportions of assets
